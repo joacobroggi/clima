79c2247eb042463e1d9380ef6292fa363a13fa03
joaco_broggi74 <playstationgta84@gmail.com>
2022-04-23 14:37:21 -0300

    ke virgen q soy

--- app.js
+++ app.js
@@ -124,6 +124,7 @@ app.get("/clima", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
