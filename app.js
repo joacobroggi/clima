3935e2e33d2d9bcb223010d90b5a94d89423bdac
joaco_broggi74 <playstationgta84@gmail.com>
2022-07-31 18:52:27 -0300

    adjustments

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+var favicon = require('serve-favicon');
+
+
 
 const app = express();
 
@@ -36,78 +39,86 @@ app.post("/", function (req, res) {
   ciudadApi = req.body.cityName;
 
   https.get(url, function (respuesta) {
-    respuesta.on("data", function (data) {
-      const wheatherData = JSON.parse(data);
+    console.log('statusCode:', respuesta.statusCode);
+    if (respuesta.statusCode == 200) {
+      respuesta.on("data", function (data) {
+        const wheatherData = JSON.parse(data);
+  
+        console.log(wheatherData.weather[0]);
+        let temp = wheatherData.main.temp;
+        let sensacion = wheatherData.main.feels_like;
+        let humedad = wheatherData.main.humidity;
+        let clima = wheatherData.weather[0].description;
+        let casoOut = wheatherData.weather[0].main;
+        let icon = wheatherData.weather[0].icon;
+        let caso = "";
+        let img = "";
+  
+        switch (casoOut) {
+          case "Clouds":
+            img = "nubes";
+            caso = "fas fa-cloud";
+            break;
+  
+          case "Rain":
+            img = "lluvia";
+            caso = "fas fa-cloud-rain";
+            break;
+          case "Thunderstorm":
+            img = "tormenta";
+            caso = "fas fa-cloud-bolt";
+            break;
+          case "Clear":
+            img = "despejado";
+            caso = "ec ec-low-brightness";
+            break;
+          case "Snow":
+            img = "nieve";
+            caso = "fas fa-snowflake";
+            break;
+          case "Haze":
+            img = "niebla";
+            caso = "fas fa-smog";
+            break;
+          default:
+            console.log("algo salio mal");
+            break;
+        }
+  
+        if (temp > 25) {
+          casoClima = "fa-solid fa-temperature-arrow-up tempAlta";
+        } else if (temp < 12) {
+          casoClima = "fa-solid fa-temperature-arrow-down tempBaja";
+        } else {
+          casoClima = "fa-solid fa-temperature-half tempMedia";
+        }
+  
+        if (sensacion > 25) {
+          casoSensacion = "ec ec-hot";
+        } else if (temp < 12) {
+          casoSensacion = "ec ec-cold";
+        } else {
+          casoSensacion = "ec ec-slightly-smiling-face";
+        }
+  
+        climaApi = clima;
+        temperaturaApi = temp;
+        sensacionApi = sensacion;
+        humedadApi = humedad;
+        casoApi = caso;
+        casoClimaApi = casoClima;
+        imgApi = img;
+        casoSensacionApi = casoSensacion;
+  
+        res.redirect("/clima");
+      });
+    } else {
+      console.log('error epico');
+      res.render("error")
+    }
 
-      console.log(wheatherData.weather[0]);
-      let temp = wheatherData.main.temp;
-      let sensacion = wheatherData.main.feels_like;
-      let humedad = wheatherData.main.humidity;
-      let clima = wheatherData.weather[0].description;
-      let casoOut = wheatherData.weather[0].main;
-      let icon = wheatherData.weather[0].icon;
-      let caso = "";
-      let img = "";
-
-      switch (casoOut) {
-        case "Clouds":
-          img = "nubes";
-          caso = "fas fa-cloud";
-          break;
-
-        case "Rain":
-          img = "lluvia";
-          caso = "fas fa-cloud-rain";
-          break;
-        case "Thunderstorm":
-          img = "tormenta";
-          caso = "fas fa-cloud-bolt";
-          break;
-        case "Clear":
-          img = "despejado";
-          caso = "ec ec-low-brightness";
-          break;
-        case "Snow":
-          img = "nieve";
-          caso = "fas fa-snowflake";
-          break;
-        case "Haze":
-          img = "niebla";
-          caso = "fas fa-smog";
-          break;
-        default:
-          console.log("algo salio mal");
-          break;
-      }
-
-      if (temp > 25) {
-        casoClima = "fa-solid fa-temperature-arrow-up tempAlta";
-      } else if (temp < 12) {
-        casoClima = "fa-solid fa-temperature-arrow-down tempBaja";
-      } else {
-        casoClima = "fa-solid fa-temperature-half tempMedia";
-      }
-
-      if (sensacion > 25) {
-        casoSensacion = "ec ec-hot";
-      } else if (temp < 12) {
-        casoSensacion = "ec ec-cold";
-      } else {
-        casoSensacion = "ec ec-slightly-smiling-face";
-      }
-
-      climaApi = clima;
-      temperaturaApi = temp;
-      sensacionApi = sensacion;
-      humedadApi = humedad;
-      casoApi = caso;
-      casoClimaApi = casoClima;
-      imgApi = img;
-      casoSensacionApi = casoSensacion;
-
-      res.redirect("/clima");
-    });
-  });
+  
+  })
 });
 
 app.get("/clima", function (req, res) {
